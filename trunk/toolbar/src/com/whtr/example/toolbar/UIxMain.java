a9abfe5292c7af7d716403427a0e0ca801eeb958
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-08-09 15:34:35 +0000

    添加toolbar基础代码

---
+++ trunk/toolbar/src/com/whtr/example/toolbar/UIxMain.java
@@ -0,0 +1,13 @@
+package com.whtr.example.toolbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UIxMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
