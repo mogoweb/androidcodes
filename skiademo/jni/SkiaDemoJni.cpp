0b4876ac7698a2180dd0a06818af914fbac3cefc
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-01 11:15:54 +0000

    增加hello skia示例

--- skiademo/jni/SkiaDemoJni.cpp
+++ skiademo/jni/SkiaDemoJni.cpp
@@ -1,2 +1,23 @@
-#include <SkiaDemoJni.h>
+#include "SkiaDemoJni.h"
+#include "mylog.h"
 
+#include "GraphicsJNI.h"
+#include "SkPaint.h"
+#include "SkCanvas.h"
+
+void Java_com_whtr_example_skiademo_SkiaView_renderHello(JNIEnv *env, jobject thizz, jobject canvas)
+{
+	LOGD("entering Java_com_whtr_example_skiademo_SkiaView_renderHello");
+
+	SkCanvas* canv = GraphicsJNI::getNativeCanvas(env, canvas);
+	if (!canv)
+	{
+		LOGE("!canv");
+		return;
+	}
+
+	SkPaint paint;
+	paint.setColor(SK_ColorRED);
+	canv->drawText("hello skia", 10, 20, 20, paint);
+
+}
