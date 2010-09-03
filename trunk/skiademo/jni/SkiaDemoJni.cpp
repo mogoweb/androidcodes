406cf948bafd56a581c1f37acbdf198ead689fae
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-03 09:20:16 +0000

    添加renderText方法

--- trunk/skiademo/jni/SkiaDemoJni.cpp
+++ trunk/skiademo/jni/SkiaDemoJni.cpp
@@ -7,8 +7,21 @@
 
 void Java_com_whtr_example_skiademo_SkiaView_renderHello(JNIEnv *env, jobject thizz, jobject canvas)
 {
-	LOGD("entering Java_com_whtr_example_skiademo_SkiaView_renderHello");
+	SkCanvas* canv = GraphicsJNI::getNativeCanvas(env, canvas);
+	if (!canv)
+	{
+		LOGE("!canv");
+		return;
+	}
 
+	SkPaint paint;
+	paint.setColor(SK_ColorRED);
+	canv->drawText("hello skia", 10, 20, 20, paint);
+
+}
+
+void Java_com_whtr_example_skiademo_SkiaView_renderText(JNIEnv *env, jobject thizz, jobject canvas)
+{
 	SkCanvas* canv = GraphicsJNI::getNativeCanvas(env, canvas);
 	if (!canv)
 	{
