299e424a9b9f8c5b5fa90e44c18d4ca40d83370c
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-03 13:59:04 +0000

    restore to r15

---
+++ skiademo/src/com/whtr/example/skiademo/SkiaView.java
@@ -0,0 +1,51 @@
+package com.whtr.example.skiademo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.View;
+
+public class SkiaView extends View
+{
+    private static final String TAG = "skiademo";
+    
+    private int mSampleId = 0;
+    
+    static 
+    {
+        System.loadLibrary("SkiaDemo");
+    }
+    
+    public native void renderHello(Canvas canvas);
+    
+    public SkiaView(Context context)
+    {
+        super(context);
+        
+        setBackgroundColor(Color.WHITE);
+    }
+    
+    public SkiaView(Context context, int sampleId)
+    {
+        this(context);
+        mSampleId = sampleId;
+    }
+    
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        
+        switch (mSampleId)
+        {
+        case 0:
+            Log.d(TAG, "render hello");
+            renderHello(canvas);
+            break;
+        default:
+            break;
+        }
+    }
+
+}
