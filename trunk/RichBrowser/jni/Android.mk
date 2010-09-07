60a7865ab855eca5ff028313874bc72f4fc07ec6
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-07 10:04:58 +0000

    add jni code fore RichBrowser

---
+++ trunk/RichBrowser/jni/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+MY_ANDROID_SOURCE=$(HOME)/android/source/cupcake
+
+LOCAL_MODULE := librichbrowser
+LOCAL_CPP_EXTENSION := .cpp
+LOCAL_CXXFLAGS :=
+
+LOCAL_C_INCLUDES := $(MY_ANDROID_SOURCE)/frameworks/base/core/jni/android/graphics \
+					$(MY_ANDROID_SOURCE)/external/skia/include/core \
+					$(MY_ANDROID_SOURCE)/external/skia/include/images \
+					$(MY_ANDROID_SOURCE)/frameworks/base/include \
+					$(MY_ANDROID_SOURCE)/system/core/include 
+
+LOCAL_SRC_FILES := BrowserJniOnLoad.cpp
+
+LOCAL_LDLIBS := -llog -lsgl -landroid_runtime \
+				-L$(MY_ANDROID_SOURCE)/out/target/product/generic/system/lib/
+
+include $(BUILD_SHARED_LIBRARY)
