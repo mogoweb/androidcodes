7100ea7a4ec1243cd3f001b6d8fa49835a3a1fa2
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-09 12:03:53 +0000

    rename FrameBridge.cpp to WebFrame.cpp

--- RichBrowser/jni/Android.mk
+++ RichBrowser/jni/Android.mk
@@ -15,9 +15,31 @@ LOCAL_C_INCLUDES := $(MY_ANDROID_SOURCE)/frameworks/base/core/jni/android/graphi
 					$(MY_ANDROID_SOURCE)/system/core/include \
 				    $(MY_ANDROID_SOURCE)/dalvik/libnativehelper/include/nativehelper 
 
-LOCAL_SRC_FILES := BrowserJniOnLoad.cpp FrameBridge.cpp
+LOCAL_SRC_FILES := BrowserJniOnLoad.cpp WebFrame.cpp
 
 LOCAL_LDLIBS := -llog -lsgl -landroid_runtime -lnativehelper \
 				-L$(MY_ANDROID_SOURCE)/out/target/product/generic/system/lib/
 
 include $(BUILD_SHARED_LIBRARY)
+
+# wtf
+include $(CLEAR_VARS)
+MY_ANDROID_SOURCE=$(HOME)/android/source/cupcake
+
+LOCAL_MODULE := libwtf
+LOCAL_CPP_EXTENSION := .cpp
+LOCAL_CXXFLAGS :=
+
+LOCAL_C_INCLUDES := $(MY_ANDROID_SOURCE)/frameworks/base/core/jni/android/graphics \
+					$(MY_ANDROID_SOURCE)/external/skia/include/core \
+					$(MY_ANDROID_SOURCE)/external/skia/include/images \
+					$(MY_ANDROID_SOURCE)/frameworks/base/include \
+					$(MY_ANDROID_SOURCE)/system/core/include \
+				    $(MY_ANDROID_SOURCE)/dalvik/libnativehelper/include/nativehelper 
+
+LOCAL_SRC_FILES := 
+
+LOCAL_LDLIBS := -llog -lsgl -landroid_runtime -lnativehelper \
+				-L$(MY_ANDROID_SOURCE)/out/target/product/generic/system/lib/
+
+include $(BUILD_STATIC_LIBRARY)
