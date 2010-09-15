7e2fd0f35da9ee5fb56b27c6b648107c844f4f83
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-15 15:18:46 +0000

    add multiLoadUrl

--- trunk/httpcurl/src/com/whtr/example/httpcurl/MainActivity.java
+++ trunk/httpcurl/src/com/whtr/example/httpcurl/MainActivity.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
     static {
-        System.loadLibrary("libhttpcurl");
+        System.loadLibrary("httpcurl");
     }
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        loadUrl("http://http://kong.net");
+        //~loadUrl("http://kong.net");
+        multiLoadUrl("http://kong.net");
     }
     
     private native void loadUrl(String url);
+    private native void multiLoadUrl(String url);
 }
