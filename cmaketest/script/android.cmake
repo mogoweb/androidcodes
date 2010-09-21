fa0d7ea456b71e8a4fa445d71590cda44a31e06b
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-21 09:18:41 +0000

    修改cmake脚本

--- cmaketest/script/android.cmake
+++ cmaketest/script/android.cmake
@@ -14,12 +14,13 @@ CMAKE_FORCE_C_COMPILER("${TOOLSROOT}/bin/arm-eabi-gcc" GNU)
 CMAKE_FORCE_CXX_COMPILER("${TOOLSROOT}/bin/arm-eabi-g++" GNU)
 
 set (CMAKE_C_FLAGS 
-		"-fpic -mthumb-interwork -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -march=armv5te -mtune=xscale -msoft-float -mthumb -Os -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -DANDROID -O2 -DNDEBUG -g -MMD -MP -MF")
+		"-fpic -mthumb-interwork -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -march=armv5te -mtune=xscale -msoft-float -mthumb -Os -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -DANDROID -O2 -DNDEBUG -g -MMD -MP ")
 
 set (CMAKE_CXX_FLAGS 
-		"-fpic -mthumb-interwork -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -march=armv5te -mtune=xscale -msoft-float -mthumb -Os -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -DANDROID -O2 -DNDEBUG -g -MMD -MP -MF")
+		"-fpic -mthumb-interwork -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -D__ARM_ARCH_5__ -D__ARM_ARCH_5T__ -D__ARM_ARCH_5E__ -D__ARM_ARCH_5TE__ -march=armv5te -mtune=xscale -msoft-float -mthumb -Os -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -DANDROID -O2 -DNDEBUG -g -MMD -MP ")
 
-set (CMAKE_SHARED_LINKER_FLAGS "-nostdlib -Wl,--no-undefined")
+set (TARGET_LIBGCC "${TOOLSROOT}/lib/gcc/arm-eabi/4.4.0/libgcc.a")
+set (CMAKE_SHARED_LINKER_FLAGS "-nostdlib -Wl,--no-undefined -Wl,-rpath-link=${TOOLSROOT}/usr/lib ${TOOLSROOT}/usr/lib/crtbegin_dynamic.o ${TARGET_LIBGCC} ${TOOLSROOT}/usr/lib/crtend_android.o")
 
 include_directories (SYSTEM "${SDKROOT}/usr/include")
 
