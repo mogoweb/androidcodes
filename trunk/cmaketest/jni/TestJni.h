78c467aec684a09eeba957303c0315a37ebb8830
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-23 14:56:14 +0000

    add TestJni

---
+++ trunk/cmaketest/jni/TestJni.h
@@ -0,0 +1,14 @@
+#ifndef __TestJni_h__
+#define __TestJni_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Java_com_whtr_example_cmaketest_MainActivity_test(JNIEnv *env, jobject thizz);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
