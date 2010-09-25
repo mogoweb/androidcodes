c5ef8592347523130d8958c87f87790472c3686c
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-09-25 11:06:28 +0000

    add native method

--- cmaketest/src/com/whtr/example/cmaketest/MainActivity.java
+++ cmaketest/src/com/whtr/example/cmaketest/MainActivity.java
@@ -4,10 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    static {
+        System.loadLibrary("test");
+    }
+    
+    private native void test();
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        test();
     }
 }
