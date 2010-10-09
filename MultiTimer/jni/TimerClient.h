8abf473720ceef8348f23788f161bb564155774d
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-10-09 02:20:49 +0000

    add some code from webkit

---
+++ MultiTimer/jni/TimerClient.h
@@ -0,0 +1,14 @@
+#ifndef TIMER_CLIENT_H
+#define TIMER_CLIENT_H
+
+class TimerClient
+{
+public:
+	virtual ~TimerClient() {}
+	virtual void setSharedTimerCallback(void(*f)()) = 0;
+	virtual void setSharedTimer(long long timemillis) = 0;
+	virtual void stopSharedTimer() = 0;
+	virtual void signalServiceFuncPtrQueue() = 0;
+};
+
+#endif
--- MultiTimer/src/com/whtr/example/mtimer/MainActivity.java
+++ MultiTimer/src/com/whtr/example/mtimer/MainActivity.java
@@ -4,10 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    static {
+        System.loadLibrary("timerdemo");
+    }
+    
+    public native void startTimer();
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        startTimer();
     }
 }
