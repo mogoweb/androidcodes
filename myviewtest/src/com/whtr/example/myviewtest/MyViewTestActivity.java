7e5848dcaab3e68b58d9b277549923fad7bea635
chen.zhengyong <chen.zhengyong@b2a4baac-cc76-cd21-6439-30fca0d695cc>
2010-12-01 08:19:12 +0000

    add myviewtest project

---
+++ myviewtest/src/com/whtr/example/myviewtest/MyViewTestActivity.java
@@ -0,0 +1,13 @@
+package com.whtr.example.myviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyViewTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
